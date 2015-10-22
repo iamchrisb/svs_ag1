315c8e8246912dfbb055864a9debbddc7d272df1
Chris B <chrismonstah@googlemail.com>
2015-10-23 00:43:38 +0200

    chr(x)

--- decrypt.py
+++ decrypt.py
@@ -1,25 +1,27 @@
-#!../svs-vm/bin/python
+#!../svs-vm/Scripts/python
 from random import shuffle
 import copy
 from random import sample
 
-plain_text = "The baroque finger returns under every poet.".lower()
-
+#plain_text = "the manager consults the attack a b c d e f g h i j k l m n o p q r s t u v w x y z".lower()
+plain_text = "Halt die Fresse krieg ein Kind".lower()
 cipher_text = ""
 
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+alphabet = []
+for i in range(32,123):
+    alphabet.append(chr(i))
 
-dict = zip(alphabet, sample(alphabet,len(alphabet)))
+print(alphabet)
 
-print(dict);
-
-for tupel1 in dict:
-    print(tupel1)
+tupleList = zip(alphabet, sample(alphabet,len(alphabet)))
 
 text_as_array = []
 text_as_array[:0] = plain_text
 
 for c1 in text_as_array:
-    
+    listIndex = alphabet.index(c1)
+    newChar = tupleList[listIndex][1]
+    cipher_text += newChar
 
+print(cipher_text)
 
